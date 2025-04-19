ffe7b938813beffb43db537f480b90cfac0c3a23
Ben Poppy <me@benpoppy.dev>
2025-04-19 09:28:31 +0100

    initial commit

---
+++ yoto/card_holder.py
@@ -0,0 +1,23 @@
+import cadquery as cq
+
+width = 100
+depth = 130
+height = 45
+wall_thickness = 5
+corner_radius = 6
+
+# Create the outer shell
+outer = cq.Workplane("XY").rect(width, depth).extrude(height)
+
+# Create the inner shell
+inner = (
+    cq.Workplane("XY")
+    .rect(width - 2 * wall_thickness, depth - 2 * wall_thickness)
+    .extrude(height - wall_thickness)
+    .translate((0, 0, wall_thickness))
+)
+
+# Remove the inner shell from outer
+holder = outer.cut(inner)
+
+show_object(holder)  # type: ignore
